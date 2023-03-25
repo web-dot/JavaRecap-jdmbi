315937ce70d1dcfc8bfdd4208ce13d3f72b6e609
sudipto <cg.sudipc@gmail.com>
2023-03-25 12:45:35 +0530

    JAVA RECAP | Exception Handling

---
+++ RecapJava.java
@@ -0,0 +1,20 @@
+public class RecapJava{
+   /*
+      source : docs.oracle.com/javase/tutorial
+   */
+
+   /*
+      1.Exception:
+         -> Exception is a short term for `Exceptional event`
+         -> All exceptions are `checked exceptions` except for those indicated by `Error` and `Runtime Exception` and their subclasses
+         -> `Checked exceptions` are subjected to catch-or-specify requirement
+         -> `Runtime exceptions` are NOT subjected to catch-or-specify requirements.    
+   **/   
+   
+   
+   
+   
+   public static void main(String[] args){
+      System.out.println("test");   
+   }
+}
